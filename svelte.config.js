a79ffcc4237501fbe02dc595682c60c217039b2c
Debashis Dip <nihan.dip@gmail.com>
2024-09-16 01:49:30 +0200

    mod icons

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
     prerender: {
       // use relative URLs similar to an anchor tag <a href="/test/1"></a>
       // do not include group layout folders in the path such as /(group)/test/1
-  entries: ['/','/movie/1', '/series/2']
+  entries: ['/','/movie/1', '/series/2', '/series/1/watch/1/1']
 }
   },
   
